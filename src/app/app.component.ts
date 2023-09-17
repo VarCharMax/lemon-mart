c3bb7fdfb5aa2860c0258db0ceff2d8c07781d17
Rohan Parkes <rparkes@email.com>
2023-09-17 17:49:52 +1000

    Fixed login interface.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatIconRegistry } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
+
+import { AuthService } from './auth/auth.service';
+
 @Component({
   selector: 'app-root',
   template: `
     <mat-toolbar color="primary">
-      <button mat-icon-button><mat-icon>menu</mat-icon></button>
+      <button *ngIf="displayAccountIcons" mat-icon-button>
+        <mat-icon>menu</mat-icon>
+      </button>
       <mat-icon svgIcon="lemon"></mat-icon>
       <a mat-button routerLink="/home"><h1>LemonMart</h1></a>
       <span class="flex-spacer"></span>
-      <button mat-mini-fab matTooltip="Profile" routerLink="/user/profile">
+      <button
+        *ngIf="displayAccountIcons"
+        mat-mini-fab
+        matTooltip="Profile"
+        routerLink="/user/profile">
         <mat-icon>account_circle</mat-icon>
       </button>
-      <button mat-icon-button routerLink="/user/logout" matTooltip="Logout">
+      <button
+        *ngIf="displayAccountIcons"
+        mat-icon-button
+        routerLink="/user/logout"
+        matTooltip="Logout">
         <mat-icon>lock_open</mat-icon>
       </button>
     </mat-toolbar>
@@ -20,12 +33,23 @@ import { DomSanitizer } from '@angular/platform-browser';
   `,
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+export class AppComponent implements OnInit {
+  displayAccountIcons: boolean = false;
+  constructor(
+    iconRegistry: MatIconRegistry,
+    sanitizer: DomSanitizer,
+    private authService: AuthService
+  ) {
     iconRegistry.addSvgIcon(
       'lemon',
       sanitizer.bypassSecurityTrustResourceUrl('assets/img/icons/lemon.svg')
     );
   }
   title = 'lemon-mart';
+
+  ngOnInit(): void {
+    this.authService.authStatus.subscribe(
+      (authStatus) => (this.displayAccountIcons = authStatus.isAuthenticated)
+    );
+  }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -14,6 +16,17 @@ import { Component } from '@angular/core';
   `,
   styles: [],
 })
-export class HomeComponent {
-  displayLogin = true;
+export class HomeComponent implements OnInit {
+  private _displayLogin = true;
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.authService.authStatus.subscribe(
+      (authStatus) => (this._displayLogin = !authStatus.isAuthenticated)
+    );
+  }
+
+  get displayLogin() {
+    return this._displayLogin;
+  }
 }
