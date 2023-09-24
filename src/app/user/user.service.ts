c14d73bb6dad7f753429f8a0e3e67c55c5b3c7ca
Rohan Parkes <rparkes@email.com>
2023-09-24 13:47:51 +1000

    Implemented User service.

---
+++ src/app/user/user.service.ts
@@ -0,0 +1,60 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, catchError } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+import { AuthService, IAuthStatus } from '../auth/auth.service';
+import { CacheService } from '../auth/cache.service';
+import { transformError } from '../common/common';
+import { IUser, User } from './user';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService extends CacheService {
+  currentUser = new BehaviorSubject<IUser>(this.getItem('user') || new User());
+  private currentAuthStatus = <IAuthStatus>{};
+  constructor(
+    private httpClient: HttpClient,
+    private authService: AuthService
+  ) {
+    super();
+    this.currentUser.subscribe((user) => this.setItem('user', user));
+    this.authService.authStatus.subscribe(
+      (authStatus) => (this.currentAuthStatus = authStatus)
+    );
+  }
+
+  getUser(id: string): Observable<IUser> {
+    return this.httpClient.get<IUser>(`${environment.baseUrl}/v1/user/${id}`);
+  }
+
+  updateUser(user: IUser): Observable<IUser> {
+    this.setItem('draft-user', user);
+
+    const updateResponse = this.httpClient
+      .put<IUser>(`${environment.baseUrl}/v1/user/${user || 0}`, user)
+      .pipe(catchError(transformError));
+
+    updateResponse.subscribe({
+      next: (res) => this.currentUser.next(res),
+      error: (err) => Observable.throw(err),
+      complete: () => this.removeItem('draft-user'),
+    });
+
+    return updateResponse;
+  }
+
+  getCurrentUser(): Observable<IUser> {
+    const userObservable = this.getUser(this.currentAuthStatus.userId).pipe(
+      catchError(transformError)
+    );
+
+    userObservable.subscribe({
+      next: (user) => this.currentUser.next(user),
+      error: (err) => Observable.throw(err),
+    });
+
+    return userObservable;
+  }
+}
