fbd62735422718236288787cb8ae46a32caae9b5
Rohan Parkes <rparkes@email.com>
2023-10-10 16:07:01 +1100

    Changed docker ci image, ci script name.

--- src/app/user/view-user/view-user.component.spec.ts
+++ src/app/user/view-user/view-user.component.spec.ts
@@ -1,6 +1,5 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { ActivatedRoute } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { MaterialModule } from 'src/app/app-material.module';
 
